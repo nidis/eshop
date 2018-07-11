6d0dc552182c7b18978d3cbf0962da89d1e2237d
Alex Symeonidis <xnidis@gmail.com>
2018-07-12 00:56:22 +0300

    hateoas changes

--- src/main/java/com/nidis/eshop/repositories/CartRepository.java
+++ src/main/java/com/nidis/eshop/repositories/CartRepository.java
@@ -5,15 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 @Transactional
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    List<Cart> findAll();
-
-    Optional<Cart> findById(Long id);
 
     Cart findBySessionIdAndIpAddress(String sessionId, String ipAddress);
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,18 @@
-insert into `product` (`description`, `price`) values ('moto g6 – 32 gb', 232.34)
-insert into `product` (`description`, `price`) values ('lg g6 – 32 gb', 242.34)
-insert into `product` (`description`, `price`) values ('samsung galaxy s8', 600.54)
-insert into `product` (`description`, `price`) values ('apple iphone 6 a1549 16gb space gray ', 174.34)
-insert into `product` (`description`, `price`) values ('blu vivo xl3 plus - 6.0”', 134.34)
-insert into `product` (`description`, `price`) values ('honor 7x - 18:9 screen ratio', 232.34)
-insert into `product` (`description`, `price`) values ('total wireless samsung galaxy j3', 232.34)
-insert into `product` (`description`, `price`) values ('tracfone lg rebel 4g lte', 232.34)
-insert into `product` (`description`, `price`) values ('blackberry key2 black unlocked', 732.34)
-insert into `product` (`description`, `price`) values ('xiaomi mi a1 (64gb, 4gb ram) ', 332.34)
+insert into `product` (`description`, `price`) values ('moto g6 – 32 gb', 232.34);
+insert into `product` (`description`, `price`) values ('lg g6 – 32 gb', 242.34);
+insert into `product` (`description`, `price`) values ('samsung galaxy s8', 600.54);
+insert into `product` (`description`, `price`) values ('apple iphone 6 a1549 16gb space gray ', 174.34);
+insert into `product` (`description`, `price`) values ('blu vivo xl3 plus - 6.0”', 134.34);
+insert into `product` (`description`, `price`) values ('honor 7x - 18:9 screen ratio', 232.34);
+insert into `product` (`description`, `price`) values ('total wireless samsung galaxy j3', 232.34);
+insert into `product` (`description`, `price`) values ('tracfone lg rebel 4g lte', 232.34);
+insert into `product` (`description`, `price`) values ('blackberry key2 black unlocked', 732.34);
+insert into `product` (`description`, `price`) values ('xiaomi mi a1 (64gb, 4gb ram) ', 332.34);
 
 ---
-insert into `customer` (`name`, `surname`, `email`, `phone`) values ('Alex', 'Symeonidis', 'a@gmail.com', '210231412')
-insert into `customer` (`name`, `surname`, `email`, `phone`) values ('John', 'Smith', 'q@gmail.com', '210541412')
+insert into `customer` (`name`, `surname`, `email`, `phone`) values ('Alex', 'Symeonidis', 'a@gmail.com', '210231412');
+insert into `customer` (`name`, `surname`, `email`, `phone`) values ('John', 'Smith', 'q@gmail.com', '210541412');
 
 ---
-insert into `address` (`customer_id`, `type`, `street`, `city`, `postcode`) values (1, 'shipping', 'Kifisias 34', 'Athens', '123 45')
+insert into `address` (`customer_id`, `type`, `street`, `city`, `postcode`) values (1, 'shipping', 'Kifisias 34', 'Athens', '123 45');
 
