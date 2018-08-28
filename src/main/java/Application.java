e041138498c75590167619ac69eab7827fdadc0e
symeonal <Alex.Symeonidis@arhs-dev-hellas.com>
2018-08-28 19:03:48 +0300

    pom update

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
